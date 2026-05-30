7215b326be27dc59f474080e37d5ff7cb42f5a33
Vikash Kumar <vikashkumar0037@gmail.com>
2026-05-30 15:56:59 +0530

    feat: initialize and sync Capacitor Android project configurations for customer, vendor, and delivery apps

---
+++ apps/customer-app/android/app/src/main/java/com/sabjiwala/customer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sabjiwala.customer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/vendor-app/android/app/src/main/java/com/sabjiwala/vendor/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sabjiwala.vendor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
